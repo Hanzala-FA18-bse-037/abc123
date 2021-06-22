b5f38c167c1ffff447f24aa66172fb9f3d763868
Hanzala-FA18-bse-037 <FA18-BSE-037@cuilahore.edu.pk>
2021-06-22 15:26:52 +0500

    comit

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-require('./database/db');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require("method-override");
-require('dotenv').config();
-var router = require('./routes/index');
-var userRouter = require('./controllers/userController');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var productsRouter = require("./routes/products");
+var usersRouter = require("./routes/users");
+var session = require("express-session");
+var sessionAuth = require("./middlewares/sessionAuth");
 var app = express();
-
+app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(sessionAuth);
+app.use(logger("dev"));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'))
-app.use('/', router);
-app.use('/', userRouter);
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", indexRouter);
+app.use("/products", productsRouter);
+app.use("/", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-
-
+mongoose
+  .connect("mongodb+srv://hanzi:hanzi@cluster0.jsjx1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo...."))
+  .catch((error) => console.log(error.message));
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,31 +1,16 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-userScehma = new mongoose.Schema({
-    name: {
-        type: String,
-        required: "Name is required."
-    },
-    gender: {
-        type: String,
-        enum: [
-            'Male', 'Female', 'Others'
-        ],
-        required: 'Gender is Required'
-    },
-    email: {
-        type: String,
-        required: 'Email is required'
-    },
-    password:{
-        type: String,
-        required: 'Please enter the password'
-    }
+var UserSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  gender: {
+    type: String,
+    enum: [
+        'Male', 'Female', 'Other'
+      ],
+    //required: 'Gender is Required'
+  },
+  password: String,
 });
-
-// Custom validation for email
-userScehma.path('email').validate((val) => {
-    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(val);
-}, 'Invalid E-mail Format.');
-const User = mongoose.model('User',userScehma);
-module.exports = User
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
